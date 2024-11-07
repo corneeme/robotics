93e4e272a2213e17236f0a6cd8ff94719ec2649e
corneeme <93950611+corneeme@users.noreply.github.com>
2024-11-06 16:32:08 -0800

    Update motortest.java

--- motortest.java
+++ motortest.java
@@ -1,10 +1,20 @@
-DcMotor lfmotor;
+DcMotor leftfront;
 
-motor = hardwareMap("motor");
+motor = hardwareMap(dc.class,"leftfront");
 
+//binds leftfront
+  if (gamepad1.a){
+    leftfront.setPower(-1);
+  } else {
+    leftfront.setPower(0);
+  }
 
+//bind to joystick 
 
+double LFpower;
+LFpower = gamepad1.right_stick_y;
 
+leftfront.setPower(LFpower);
 
 
 
